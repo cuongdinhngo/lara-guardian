4268f10c994d4e5daeeb085883368217a1d456bf
Ngo Dinh Cuong <cuongnd@nitrotech.asia>
2021-07-17 16:42:15 +0700

    query kit

--- src/Models/Action.php
+++ src/Models/Action.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use App\Guardian\Traits\QueryKit;
+use App\Traits\QueryKit;
 
 class Action extends Model
 {
